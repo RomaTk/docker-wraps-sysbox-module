365533d0d3aa3736f95739b797b054bbed1bc48b
RomaTk <rt.craz@gmail.com>
2025-10-31 18:22:23 +0000

    Sysbox fix install on local machine (#2)
    * Refactor sysbox installation script to dynamically read version and architecture
    
    * Fix syntax in clean and run commands in envs.json for better readability
    
    * Refactor main.sh to improve error handling and update envs.json to source the script correctly

--- dockers/sysbox/get-latest-version/main.sh
+++ dockers/sysbox/get-latest-version/main.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+function main {
+    local latest_version
+    latest_version=$(getVersion)
+    if [ $? -ne 0 ]; then
+        echo "Error getting latest version"
+        exit 1
+    fi
+
+    echo "LATEST_VERSION=\"$latest_version\"" > /working-env/sysbox/configs/latest-version.cfg
+    if [ $? -ne 0 ]; then
+        echo "Error writing latest version to config file"
+        exit 1
+    fi
+
+    exit 0
+}
 
 function getVersion {
     local api_url
@@ -32,7 +48,3 @@ function getVersion {
 
     exit 0
 }
-
-latest_version=$(getVersion)
-
-echo "LATEST_VERSION=\"$latest_version\"" > /working-env/sysbox/configs/latest-version.cfg
