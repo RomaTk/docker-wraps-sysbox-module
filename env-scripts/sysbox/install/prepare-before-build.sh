e4095883f90c59faf43393489cfca7fec3da9211
romaTk <rt.craz@gmail.com>
2025-11-04 16:09:07 +0000

    Add isRunDownloadWrap function to check for existing downloaded versions

--- env-scripts/sysbox/install/prepare-before-build.sh
+++ env-scripts/sysbox/install/prepare-before-build.sh
@@ -24,6 +24,20 @@ function main {
     exit 0
 }
 
+function isRunDownloadWrap {
+    local version="$1"
+    local os="$2"
+    local arch="$3"
+
+    if [ -f "./dockers/sysbox/install/saved-versions/${arch}-${version}.deb" ]; then
+        echo "no"
+        exit 0
+    fi
+
+    echo "yes"
+    exit 0
+}
+
 function formatArch {
     local arch="$1"
 
